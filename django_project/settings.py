295e5107447fcbf7b4055b4207d732df62972f1c
caesarning <ymq4dn@virginia.edu>
2022-07-24 19:11:54 -0400

    try

--- django_project/settings.py
+++ django_project/settings.py
@@ -151,22 +151,12 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
-EMAIL_USER = os.environ.get('EMAIL_USER')
-EMAIL_PASS = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASS = os.environ.get('EMAIL_PASS')
 
 AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-# EMAIL_USER = os.environ.get['EMAIL_USER']
-# EMAIL_PASS = os.environ.get['EMAIL_PASS']
-
-# AWS_ACCESS_KEY_ID= os.environ.get['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY= os.environ.get['AWS_SECRET_ACCESS_KEY']
-# AWS_STORAGE_BUCKET_NAME= os.environ.get['AWS_STORAGE_BUCKET_NAME']
-
-
-
 
 
 AWS_S3_FIE_OVERWRITE = False
