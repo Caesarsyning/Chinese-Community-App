d836af8902242ebed8c381f9403f5aa6376d1407
caesarning <ymq4dn@virginia.edu>
2022-07-28 12:47:19 -0400

    confirm ssl

--- django_project/urls.py
+++ django_project/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('profile/',user_views.profile,name="profile"),
     path('',include('blog.urls')),
     path('.well-known/acme-challenge/iTTf0SsOPg20F3RC623k8ps5KyHWgcPKQq6buHfT068/',
-        blog_views.confirm,name='confirm')
+        blog_views.confirm,name='confirm'),
     # django provides login and logout view(form) for us, but we still need to implement the html template
     path('login/',auth_views.LoginView.as_view(template_name ='users/login.html'),name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name ='users/logout.html'),name="logout"),
