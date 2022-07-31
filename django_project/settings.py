68a99431ca53b2b1714a811d98867d260308d29b
caesarning <ymq4dn@virginia.edu>
2022-07-31 17:25:21 -0400

    debug updte

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG=(os.environ.get(DEBUG_VALUE)=='True')
 # DEBUG=(os.environ.get[DEBUG_VALUE]=='True')
 # DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['caesar-first-web-app.herokuapp.com','127.0.0.1']
 
