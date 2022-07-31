8fdff0e681d8c6ba025dc225ba3983e1544f856d
caesarning <ymq4dn@virginia.edu>
2022-07-31 18:51:18 -0400

    test blog

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG=(os.environ.get(DEBUG_VALUE)=='True')
 # DEBUG=(os.environ.get[DEBUG_VALUE]=='True')
-# DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
+warnings.filterwarnings("ignore", message="No directory at", module="whitenoise.base" )
 ALLOWED_HOSTS = ['caesar-first-web-app.herokuapp.com']
 
 
