a175780f1442b21678d89f079ec09bd9fb552720
caesarning <ymq4dn@virginia.edu>
2022-07-31 21:40:20 -0400

    add white noise_use_finder

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
-DEBUG = True
+DEBUG = False
 WHITENOISE_USE_FINDERS = True
 warnings.filterwarnings("ignore", message="No directory at", module="whitenoise.base" )
 ALLOWED_HOSTS = ['caesar-first-web-app.herokuapp.com']
