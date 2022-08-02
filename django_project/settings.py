e073a9a6347bad2fae95f19080380fa19db3e1f0
caesarning <ymq4dn@virginia.edu>
2022-08-02 00:52:39 -0400

    modify debug value

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
+
 
 WHITENOISE_USE_FINDERS = True
 # warnings.filterwarnings("ignore", message="No directory at", module="whitenoise.base" )
