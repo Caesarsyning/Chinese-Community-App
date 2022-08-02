8bcc22c0e8fcdbc1eec82f129a63b26d2de6c333
caesarning <ymq4dn@virginia.edu>
2022-08-02 00:47:06 -0400

    modify urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,39 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
-from users import views as user_views
-from blog import views as blog_views
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
 from django.conf.urls.static import static
-from home import views as home_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('register/',user_views.register,name="register"),
-    path('profile/',user_views.profile,name="profile"),
-    # path('',include('blog.urls')),
-    path('', login_required(home_view.home),name='home'),
-    # path('',include('home.urls')),
-    # django provides login and logout view(form) for us, but we still need to implement the html template
-    # quote for customize loginView: https://www.youtube.com/watch?v=8V-mscw6H64
-    path('login/',auth_views.LoginView.as_view(template_name ='user/login.html'),name="login"),
-    path('logout/',user_views.logout_view,name="logout"),
-    path('password-reset/',
-        auth_views.PasswordResetView.as_view(template_name ='user/password_reset.html'),
-        name="password_reset"),
-    path('password-reset/done',
-        auth_views.PasswordResetDoneView.as_view(template_name ='user/password_reset_done.html'),
-        name="password_reset_done"),
-    path('password-reset-confirm/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(template_name ='user/password_reset_confirm.html'),
-        name="password_reset_confirm"),
-    path('password-reset-complete/',
-        auth_views.PasswordResetCompleteView.as_view(template_name ='user/password_reset_complete.html'),
-        name="password_reset_complete")
-    
+    path('',include('home.urls')),
+    path('users/',include('users.urls')),
 ]
+
 
 
 if settings.DEBUG:
