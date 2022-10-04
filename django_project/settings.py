783bfcff53d036811e808ad19336ff481c571c11
Siyuan Ning (Caesar) <107768975+Caesarsyning@users.noreply.github.com>
2022-10-04 02:26:33 -0400

    add housing101

--- django_project/settings.py
+++ django_project/settings.py
@@ -296,7 +296,7 @@ AWS_S3_FIE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 
 
